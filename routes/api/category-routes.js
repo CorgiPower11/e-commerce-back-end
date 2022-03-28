d41723b7e9108e82b75d7d44233b49ab7c8d9cd3
Kathy Moua <kathy@page-ed.org>
2022-03-27 23:58:03 -0500

    ReadMe

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,7 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  C.findAll ({
+  Category.findAll ({
     include: [
       {
         model: Category,
